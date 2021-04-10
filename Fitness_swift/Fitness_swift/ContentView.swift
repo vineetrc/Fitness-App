5e7c7913f4837f4cfe8dce58ba875baf2897aebe
AbhigyaWangoo <abhigyawangoo@gmail.com>
2021-04-10 09:27:33 -0700

    Added starter swift files

---
+++ Fitness_swift/Fitness_swift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Fitness_swift
+//
+//  Created by Abhi Wangoo on 4/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Fitness_swift/Fitness_swift/Fitness_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fitness_swiftApp.swift
+//  Fitness_swift
+//
+//  Created by Abhi Wangoo on 4/10/21.
+//
+
+import SwiftUI
+
+@main
+struct Fitness_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
