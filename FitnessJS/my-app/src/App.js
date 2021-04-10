f30b4fae73db0da49ff692e3a62851116efd6cbc
vineetrc <vineetreddi@gmail.com>
2021-04-10 17:17:09 -0500

    add folds

---
+++ FitnessJS/my-app/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h4>Welcome to the Fitness Program</h4>
+      <button type="button" class="btn btn-primary">Click Here to Add your workout</button>
+      <img src="https://images.unsplash.com/photo-1517836357463-d25dfeac3438?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Nnx8Zml0bmVzc3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80" alt="Logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
