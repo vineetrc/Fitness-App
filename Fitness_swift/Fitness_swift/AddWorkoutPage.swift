11fbfa75e03584fdfe24c03429bf519e7c80e498
AbhigyaWangoo <abhigyawangoo@gmail.com>
2021-04-10 10:03:11 -0700

    Added basic UI skeleton

---
+++ Fitness_swift/Fitness_swift/AddWorkoutPage.swift
@@ -0,0 +1,30 @@
+//
+//  AddWorkoutPage.swift
+//  Fitness_swift
+//
+//  Created by Abhi Wangoo on 4/10/21.
+//
+
+import SwiftUI
+
+struct AddWorkoutPage: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Spacer()
+                Button(action: {
+                
+                }) {
+                    Text("Add workout button")
+                        
+                }.padding()
+            }
+        }
+    }
+}
+
+struct AddWorkoutPage_Previews: PreviewProvider {
+    static var previews: some View {
+        AddWorkoutPage()
+    }
+}
---
+++ Fitness_swift/Fitness_swift/CurrentWorkoutsPage.swift
@@ -0,0 +1,24 @@
+//
+//  CurrentWorkoutsPage.swift
+//  Fitness_swift
+//
+//  Created by Abhi Wangoo on 4/10/21.
+//
+
+import SwiftUI
+
+struct CurrentWorkoutsPage: View {
+    var body: some View {
+        VStack {
+            Text("Current workouts")
+            Spacer()
+            Text("Current workouts go here")
+        }
+    }
+}
+
+struct CurrentWorkoutsPage_Previews: PreviewProvider {
+    static var previews: some View {
+        CurrentWorkoutsPage()
+    }
+}
